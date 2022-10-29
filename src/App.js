dffc59efa6d79a135f4a25504bc4504e11ca5fbb
Tolu-Mals <malomo.tolz@gmail.com>
2022-10-29 13:31:52 +0100

    Add links section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Container } from '@chakra-ui/react';
 import Profile from './components/Profile';
+import Links from './components/Links';
 
 function App() {
   return (
     <Container maxWidth="1216px" py="44px">
       <Profile />
+      <Links />
     </Container>
   );
 }
