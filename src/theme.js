6ddfd4e33441a1938fd1a5e8ed2513dbcf2e440a
Tolu-Mals <malomo.tolz@gmail.com>
2022-10-29 12:44:32 +0100

    Customize theme color & Heading base style

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,28 @@
 import { extendTheme } from "@chakra-ui/react";
 
-const theme = extendTheme({
-  fonts: {
-    heading: "'Inter', sans-serif",
-    body: "'Inter', sans-serif",
+const components = {
+  Heading: {
+    baseStyle: {
+      color: 'gray.900',
+    }
   },
+}
+
+const fonts = {
+  heading: "'Inter', sans-serif",
+  body: "'Inter', sans-serif",
+}
+
+const colors = {
+  gray: {
+    900: '#101828'
+  }
+}
+
+const theme = extendTheme({
+  fonts,
+  colors,
+  components
 })
 
 export default theme
