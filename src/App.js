a58c72544668f749b0faa77658eeaecb3e2b710b
Tolu-Mals <malomo.tolz@gmail.com>
2022-10-29 14:21:11 +0100

    Add social links

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Container } from '@chakra-ui/react';
 import Profile from './components/Profile';
 import Links from './components/Links';
+import SocialLinks from './components/SocialLinks';
 
 function App() {
   return (
     <Container maxWidth="1216px" py="44px">
       <Profile />
       <Links />
+      <SocialLinks />
     </Container>
   );
 }
