73d2d7a82dd648acb3d182b0a8a10ec1d00d0257
Tolu-Mals <malomo.tolz@gmail.com>
2022-10-30 12:55:05 +0100

    Make share button responsive

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { chakra, Image, Box, Heading, VisuallyHidden } from '@chakra-ui/react';
 import OptionsIcon from '../assets/icons/options-icon.png';
+import ShareIcon from '../assets/icons/share-icon.png';
 import ProfileImg from '../assets/img/profile-img.png';
 
 const OptionsButton = (
-  <chakra.button aria-label="Options" ml="auto" display="block" w="40px" h="40px" mb="-20px">
+  <chakra.button 
+  aria-label="Options" 
+  ml="auto" 
+  display={{ base: "block", md: "none" }} 
+  w="40px" 
+  h="40px" 
+  mb="-20px"
+  >
     <img src={OptionsIcon} alt="Options icon"/>
+  </chakra.button>
+)
+
+const ShareButton = (
+  <chakra.button 
+  aria-label="Options" 
+  ml="80%" 
+  display={{ base: "none", md: "block" }} 
+  w="40px" 
+  h="40px" 
+  mb="-20px"
+  >
+    <img src={ShareIcon} alt="Share icon"/>
   </chakra.button>
 )
 
@@ -13,7 +34,7 @@ const Profile = () => {
   return (
     <chakra.header mb={{ base: "16px", md: "32px" }}>
       { OptionsButton }
-
+      { ShareButton }
       <Box boxSize="88px" mx="auto" mb={6}>
         <Image src={ProfileImg} alt='Profile Picture' id="profile__img" />
       </Box>
