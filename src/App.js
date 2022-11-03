1f18967a0d771ce49c24aef7cbf9e71ddc16ba0c
Tolu-Mals <malomo.tolz@gmail.com>
2022-11-03 13:11:37 +0100

    refactor: create home page component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import { Container } from '@chakra-ui/react';
-import Profile from './components/Profile';
-import Links from './components/Links';
-import SocialLinks from './components/SocialLinks';
-import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <Container maxWidth="1216px" py="44px">
-      <Profile />
-      <Links />
-      <SocialLinks />
-      <Footer />
-    </Container>
+    <Home />
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container } from '@chakra-ui/react';
+import Profile from '../components/Profile';
+import Links from '../components/Links';
+import SocialLinks from '../components/SocialLinks';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <Container maxWidth="1216px" py="44px">
+      <Profile />
+      <Links />
+      <SocialLinks />
+      <Footer />
+    </Container>
+  )
+}
+
+export default Home
