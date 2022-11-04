ccbf9e2488051d57337774416cb1847097869cef
Tolu-Mals <malomo.tolz@gmail.com>
2022-11-04 13:37:35 +0100

    feat: add custom checkbox component

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,15 +1,44 @@
 import React, { useId, useState } from 'react';
 import { chakra, Flex } from '@chakra-ui/react';
+import checkIcon from '../assets/icons/check-icon.png';
 
-const Checkbox = ({ children, spacing="16px" }) => {
+const Checkbox = ({ children, spacing="16px", ...rest }) => {
   const id = useId();
   const [isChecked, setIsChecked] = useState(false);
 
   const checkboxStyle = {
-    width: '24px',
-    height: '24px',
+    minWidth: '20px',
+    minHeight: '20px',
     backgroundColor: '#fff',
-    border: '1px solid black',
+    borderRadius: '6px',
+    border: isChecked ? '1px solid #1570EF' : '1px solid #D0D5DD',
+    backgroundImage: isChecked ? `url(${checkIcon})` : '',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
+    backgroundSize: '14px 14px',
+    maxWidth: '20px',
+    maxHeight: '20px',
+    boxShadow: 'none',
+    flex: 1,
+    _dataFocusVisible: {
+      outline: 'none',
+      border: '1px solid #84CAFF',
+      boxShadow: '0px 0px 0px 4px #D1E9FF',
+    },
+    _focusVisible: {
+      outline: 'none',
+      border: '1px solid #84CAFF',
+      boxShadow: '0px 0px 0px 4px #D1E9FF',
+      zIndex: 1,
+    },
+    _blur: {
+      boxShadow: 'none',
+    },
+    _active: {
+      outline: 'none',
+      boxShadow: 'none',
+      border: 'none'
+    }
   };
 
   const handleClick = () => {
@@ -17,8 +46,8 @@ const Checkbox = ({ children, spacing="16px" }) => {
   }
 
   return (
-    <Flex>
-      <chakra.div role="checkbox" sx={checkboxStyle} id={`checkbox-${id}`} onClick={handleClick} mr={spacing}></chakra.div>
+    <Flex {...rest}>
+      <chakra.div role="checkbox" sx={checkboxStyle} id={`checkbox-${id}`} onClick={handleClick} mr={spacing} tabIndex={"0"}></chakra.div>
       <label>{ children }</label>
     </Flex>
   )
