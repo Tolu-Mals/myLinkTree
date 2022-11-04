56a77dfd9c552c387fc86e9870cac122ee3ccbb7
Tolu-Mals <malomo.tolz@gmail.com>
2022-11-04 13:39:00 +0100

    feat: add contact page 2

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,32 +9,59 @@ import {
   Textarea, 
   Button,
   FormLabel,
+  Box,
+  VStack,
+  Flex
 } from '@chakra-ui/react';
 import Footer from '../components/Footer';
 import Checkbox from '../components/Checkbox';
 
+const WideBox = ({ children }) => (
+  <Box w="100%">{children}</Box>
+);
+
 const Contact = () => {
   return (
-    <Container maxWidth="1216px" py="44px">
+    <Container maxWidth="720px" py="44px">
       <chakra.header>
-        <Heading>Contact Me</Heading>
-        <Text>Hi there, contact me to ask me about anything you have in mind.</Text>
+        <Heading fontSize="36px" mb={4} color="gray.900">Contact Me</Heading>
+        <Text fontSize="18px" color="gray.500" mb={12}>Hi there, contact me to ask me about anything you have in mind.</Text>
 
         <FormControl as="form">
-          <FormLabel htmlFor='first_name'>First name</FormLabel>
-          <Input type="text" id="first_name" placeholder='Enter your first name' />
+          <VStack textAlign={"left"} spacing={6} mb={8}>
 
-          <FormLabel htmlFor='last_name'>Last name</FormLabel>
-          <Input type="text" id="last_name" placeholder='Enter your last name' />
+            <Flex w="100%" gap="24px" sx={{
+              '@media screen and (max-width: 450px)': {
+                flexDirection: 'column',
+              }
+            }}>
+              <WideBox w="100%">
+                <FormLabel htmlFor='first_name'>First name</FormLabel>
+                <Input type="text" id="first_name" placeholder='Enter your first name' />
+              </WideBox>
 
-          <FormLabel htmlFor='email'>Email</FormLabel>
-          <Input type="email" id="email" placeholder='yourname@email.com' />
+              <WideBox>
+                <FormLabel htmlFor='last_name'>Last name</FormLabel>
+                <Input type="text" id="last_name" placeholder='Enter your last name' />
+              </WideBox>
+            </Flex>
 
-          <FormLabel htmlFor='message'>Message</FormLabel>
-          <Textarea placeholder="Send me a message and I'll reply you as soon as possible..." id="message" py={3} px="14px" />
+            <WideBox>
+              <FormLabel htmlFor='email'>Email</FormLabel>
+              <Input type="email" id="email" placeholder='yourname@email.com' />
+            </WideBox>
 
-          <Checkbox>I agree to all terms</Checkbox>
-          <Button id="btn__submit" width="100%">Send Message</Button>
+            <WideBox>
+              <FormLabel htmlFor='message'>Message</FormLabel>
+              <Textarea placeholder="Send me a message and I'll reply you as soon as possible..." id="message" py={3} px="14px" />
+            </WideBox>
+
+            <Checkbox spacing="8px" alignItems={"start"} w="100%">
+              You agree to providing your data to Tolulope who may contact you.
+            </Checkbox>
+          </VStack>
+
+          <Button id="btn__submit" width="100%" variant="action">Send Message</Button>
         </FormControl>
       </chakra.header>
       <Footer />
